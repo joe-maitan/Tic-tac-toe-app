f0816c8fff9f5fc19f9692a29e09a1d1d8ee61af
jjmaitan <jjmaitan@colostate.edu>
2024-10-22 15:13:17 -0600

    Crafting the empty app

--- backend/models.py
+++ backend/models.py
@@ -4,5 +4,5 @@ class User(db.model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=False, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), unique=False, nullable=False)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
+import PlayOnline from './Components/PlayOnline'
+import PlayOffline from './Components/PlayOffline'
 
 export default function App() {
   return (
     <div>
       <h1>TicTacToe.com</h1>
+      <SignIn></SignIn>
       <PlayOnline/>
       <PlayOffline/>
     </div>
