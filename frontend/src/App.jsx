e1f82235f4be9f142c95809614316b126204b89f
jjmaitan <jjmaitan@colostate.edu>
2024-10-22 11:39:44 -0600

    Add necessary project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <h1>THIS IS FROM APP.JSX!</h1>
+    </div>
+  );
+}; // End App() function
