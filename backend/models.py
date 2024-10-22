92e7e4936442153088d8c5402719c3f85b83e8a5
jjmaitan <jjmaitan@colostate.edu>
2024-10-22 16:37:38 -0600

    Found error fetching request

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from config import db
 
-class User(db.model):
+class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
