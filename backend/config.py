0b44d51685036b9ba0c27dc6cf2fa8d5b0ba8100
jjmaitan <jjmaitan@colostate.edu>
2024-10-24 13:45:18 -0600

    Working on database config

--- backend/config.py
+++ backend/config.py
@@ -1,27 +1,33 @@
+import pymongo
+
+from pymongo import MongoClient
+
 from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from pymongo.mongo_client import MongoClient
+
 
 app = Flask(__name__)
 CORS(app)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# MongoDB configuration
-#app.config['MONGO_URI'] = 'mongodb://localhost:27017/tictactoe'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/'
-
 # db = SQLAlchemy(app)
 
-# Initialize MongoDB client
-client = MongoClient(app.config['MONGO_URI'])
-db = client.get_database()
+
+app.logger.info("congfig.py - Flask app created")
+app.logger.info("congfig.py - Establishing connection to database")
+
+# MongoDB configuration
+client = MongoClient('mongodb://localhost:27017/')
+db = client['TestTicTacToeDB']
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/'
+# app.config['MONGO_DBNAME'] = 'TestTicTacToeDB'
+
 
 try:
     client.admin.command('ping')
-    app.logger.info("Connected to database")
+    app.logger.info("congfig.py - Successfully connected to database.")
 except Exception as e:
-    app.logger.error("Error connecting to database")
+    app.logger.error("congfig.py - Error connecting to database.")
     app.logger.error(e)
