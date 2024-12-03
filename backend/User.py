3ef085b8ff058741079ed6a4b5e76e78b7487270
Joe <92334673+joe-maitan@users.noreply.github.com>
2024-12-03 14:09:37 -0700

    Added Comments for the User.py file

--- backend/User.py
+++ backend/User.py
@@ -1,39 +1,49 @@
+##
+# @file User.py
+# 
+# @brief Defines the user class.
+# 
+# This is the class for how we keep track of who is doing what on the backend.
+
 class User():
 
+    # User constructor, created whenever a user signs up or logs in
     def __init__(self, is_authenticated, user_id):
         self._id = user_id  # this is the username of the user. Which can be used to identify them in the DB and here
         self._is_authenticated = is_authenticated
         self._is_active = True
         self._is_anonymous = False
-        self.game_symbol = ''  # this will be used to store the game symbol of the user
     
 
+    # Necessary property for working with Flask
     @property
     def is_authenticated(self):
         return self._is_authenticated
     
 
+    # Necessary property for working with Flask
     @property
     def is_active(self):
         return self._is_active
     
 
+    # Necessary property for working with Flask
     @property
     def is_anonymous(self):
         return self._is_anonymous
 
 
+    # get_id()
+    # @param None
+    # @return The username of the user object, which is what we use to uniquely identify
+    # each user.
     def get_id(self):
-        return self._id  # returns the username of the user
+        return self._id
     
 
+    # get_is_authenticated()
+    # @param None
+    # @return A boolean value if the user is authenticated or not.
     def get_is_authenticated(self):
         return self._is_authenticated
     
-
-    def set_symbol(self, symbol):
-        self.game_symbol = symbol
-
-    
-    def get_symbol(self):
-        return self.game_symbol
